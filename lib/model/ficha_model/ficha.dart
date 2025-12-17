a92bd724eb7282bb66ccdc29569f5f05962d76f5
kakeven <kevsnsilba@gmail.com>
2025-12-17 17:51:19 -0300

    teste3

--- lib/model/ficha_model/ficha.dart
+++ lib/model/ficha_model/ficha.dart
@@ -1,9 +1,45 @@
+class Componente{
+  final String nomeComponente;
+  final String efeito;
+  final int graduacao;
+  final int custoBase;
+  final Map<String, int> extras;
+  final Map<String, int> falhas;
+
+  Componente({
+    required this.nomeComponente,
+    required this.efeito,
+    required this.graduacao,
+    required this.custoBase,
+    Map<String, int>? extras,
+    Map<String, int>? falhas,
+  })  : extras = extras ?? {},
+        falhas = falhas ?? {};
+
+}
+
+class Poder{
+  final String nomePoder;
+  final Map<String,int> extras;
+  final Map<String,int> falhas;
+
+  Poder({
+    required this.nomePoder,
+    Map<String, int>? extras,
+    Map<String, int>? falhas,
+  })  : extras = extras ?? {},
+        falhas = falhas ?? {};
+  }
+
 class Ficha{
 
-  int poder = 10;
+  final int np ;
+  final String nomeJogador;
+  final String nomePersonagem;
 
 
 
+  Ficha(this.np,this.nomeJogador,this.nomePersonagem);
 
 
 }
