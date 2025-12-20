bd3d8936ac6f9b3a0d99f50b2330fffb9c775a4a
Mr-Tetin <raimundo.porto5566@gmail.com>
2025-12-20 16:03:58 -0300

    Adicionado tela da ficha e botao de ficha funcionando

--- lib/pages/tela_ficha.dart
+++ lib/pages/tela_ficha.dart
@@ -7,6 +7,50 @@ class TelaFicha extends StatelessWidget{
   Widget build(BuildContext context){
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 21, 22, 34),
+      body: Align(
+        alignment: Alignment.topCenter,
+        child: Padding(
+          padding: const EdgeInsets.only(top: 20),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              //***Titulo***
+              Text("Fichas",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 50,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+
+
+              //***espaçamento***
+              const SizedBox(height: 40), 
+
+
+              //***Botao CRIAR FICHA***
+              ElevatedButton(
+
+                //tamanho botao
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(300, 100)
+                ),
+
+                //funcao botao
+                onPressed: () {},
+
+                //texto botao
+                child: const Text(
+                  "Criar ficha",
+                  style: TextStyle(
+                    fontSize: 25,
+                  ),
+                ),
+              )
+            ],
+          ),
+        )
+      ),
     );
   }
 }
