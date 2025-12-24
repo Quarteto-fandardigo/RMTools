db897c178d7c628c67ca63cbcec36a8a53b9e29f
Mr-Tetin <raimundo.porto5566@gmail.com>
2025-12-24 15:07:41 -0300

    fazendo a tela e habilidades

--- lib/pages/tela_habilidades.dart
+++ lib/pages/tela_habilidades.dart
@@ -16,47 +16,118 @@ class _TelaHabilidades extends State<TelaHabilidades>{
   Widget build(BuildContext context){
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 21, 22, 34),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 25),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            IconButton(
-              icon: Icon(Icons.help),
-              iconSize: 35,
-              color: Colors.white,
-              onPressed: () {},
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 25),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.help),
+                  iconSize: 35,
+                  color: Colors.white,
+                  onPressed: () {},
+                ),
+                
+
+                //***FutureBuilder para ele ler somente quando o arquivo for escrito, ou seja, dps
+                FutureBuilder<int?>(
+                  future: FichaRepository().carregarCampoInt("pontosD", widget.nomePersonagem),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const SizedBox(
+                        width: 20,
+                        height: 20,
+                        child: CircularProgressIndicator(strokeWidth: 2),
+                      );
+                    }
+
+
+                    final pontos = snapshot.data ?? 0;
+
+                    //***Texto Pontos Disponíveis***
+                    return Text(
+                      "Pontos disponíveis: $pontos",
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    );
+                  },
+                ),
+              ],
             ),
-            
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
 
-            //***FutureBuilder para ele ler somente quando o arquivo for escrito, ou seja, dps
-            FutureBuilder<int?>(
-              future: FichaRepository().carregarCampoInt("pontosD", widget.nomePersonagem),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const SizedBox(
-                    width: 20,
-                    height: 20,
-                    child: CircularProgressIndicator(strokeWidth: 2),
-                  );
-                }
+                  //***Força***
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        
+                        //Texto stepper Força
+                        Text(
+                          "Força:",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 40,
+                          ),
+                        ),
+                        
 
+                        //***Stepper***
+                        Row(
+                          children: [
 
-                final pontos = snapshot.data ?? 0;
+                            //Adicionar
+                            IconButton(
+                              icon: Icon(Icons.remove, color: Colors.white),
+                              onPressed: () {},
+                              iconSize: 30,
+                            ),
+                            
+                            //Texto
+                            Text(
+                              "10",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 40,
+                              ),
+                            ),
 
-                //***Texto Pontos Disponíveis***
-                return Text(
-                  "Pontos disponíveis: $pontos",
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.w500,
+                            //Remover
+                            IconButton(
+                              icon: Icon(Icons.add, color: Colors.white),
+                              onPressed: () {},
+                              iconSize: 30,
+                            )
+                          ],
+                        )
+                      ],
+                    )
                   ),
-                );
-              },
+
+
+
+
+
+
+
+
+
+
+                ],
+              ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
